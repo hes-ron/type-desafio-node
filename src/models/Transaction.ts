bb6a424101f5d2138c6e35bde2988073e5f34984
Hesron <hesronbressiani@hotmail.com>
2020-11-24 23:46:36 -0300

    all challenge done

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,40 @@
-class Transaction {
-  id: string;
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn
+} from 'typeorm';
 
+import Category from './Category';
+
+@Entity('transactions')
+class Transaction {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
-  category_id: string;
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' }) 
+  category: Category;
 
-  created_at: Date;
+  @Column()
+  category_id: string
 
+  @CreateDateColumn()
+  created_at: Date; 
+
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
